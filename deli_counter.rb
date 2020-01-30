f371f6b12ce34f8f413e7b996ab0d8343ffa2220
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 14:07:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each.with_index(1) do |name, index|
-      customers = "The line is currently: #{index + 1}. #{name}"
+      puts customers = "The line is currently: #{index + 1}. #{name}"
       customer.push("#{index}. #{name}")
     end
       current_line + customer.join(" ")
