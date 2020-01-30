a6e76a525c8b7afe644028baf860c1e216127a93
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 14:30:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,9 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{index + 1} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli.empty?
+
+  end
 
 end
