d47af95f9225cfcfab362141f6f18e648b1ce251
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 14:26:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.empty?
-    index = katz_deli.length 
+    index = katz_deli.length
     katz_deli << name
     puts "Welcome, #{name}. "
   end
